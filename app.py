d2e664e4f76b9f929f3d457e98449a1a4a0d4f2f
akshay1290 <95462092+akshay1290@users.noreply.github.com>
2023-03-28 18:19:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.combine import SMOTEENN
 
 
 from sklearn import preprocessing
